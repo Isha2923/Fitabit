ce0fd131cf9d6156d8e6ec51d2457c02d6f3583b
ISHA SHARMA <122515895+Isha2923@users.noreply.github.com>
2025-04-27 12:15:44 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(express.json()); //pass incoming json data from the user
 const corsOptions = {
   //origin: ["http://localhost:5173"], //!write local website running link, server running on port 8000, port no and actual host no - both are diff things
   //origin: "https://fitabit.vercel.app",
-  origin: "https://fitabit-habit-of-fitness.vercel.app/",
+  origin: "https://fitabit-habit-of-fitness.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
